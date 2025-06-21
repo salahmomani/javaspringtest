e0d1e9dcc1daae8957e1a78778d2b50256a6df42
salah almomani <slhalmomani@gmail.com>
2025-06-22 00:57:14 +0300

    repo change

--- springrest/src/main/java/com/salah/springRest/repo/VendorRepo.java
+++ springrest/src/main/java/com/salah/springRest/repo/VendorRepo.java
@@ -3,7 +3,7 @@ package com.salah.springRest.repo;
 import com.salah.springRest.model.VendorCloud;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-//salah
+//sa dlah
 @Repository
 public interface VendorRepo extends JpaRepository<VendorCloud,String> {
 }
